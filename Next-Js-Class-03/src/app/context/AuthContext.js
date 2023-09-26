e628ee207f4789fc89bb3748abf255974c3caa1f
junaidsalam639 <junaidsalam639@gmail.com>
2023-09-26 23:05:21 +0500

    'true'

--- Next-Js-Class-03/src/app/context/AuthContext.js
+++ Next-Js-Class-03/src/app/context/AuthContext.js
@@ -5,7 +5,7 @@ export const Auth = createContext();
 
 export default function AuthContext ({children}){
     const [user , setUser] = useState({
-        userName : 'Junaid',
+        name : 'Junaid',
         age  : '20',
         class : 'web and App'
     });
--- Next-Js-Class-03/src/app/page.js
+++ Next-Js-Class-03/src/app/page.js
@@ -5,11 +5,30 @@ import { useContext } from 'react'
 export default function Home() {
   const {theme , setTheme} = useContext(Theme);
   const {user , setUser} = useContext(Auth);
-  console.log(user.userName);
+
+  const userInfoChange = () => {
+    if(user.age == 20){
+      setUser({
+        name : 'Faiz',
+        age : 25,
+        class : 'Necessary'
+      })
+    }else {
+      setUser({
+        name : 'Atta',
+        age : 20,
+        class : 'KGS-I'
+      })
+    }
+  }
+  
   return (
     <>
-    <h1 className='text-center text-3xl font-bold bg-indigo-800 text-indigo-50 py-5'>Name :{user.userName}: Age : {user.age} : Class : {user.class} </h1>
-    <h1 className='text-center text-3xl font-bold text-indigo-50 py-5 my-4' style={{backgroundColor : theme}} onClick={() => theme == 'red' ? setTheme('black') : setTheme('red')}>Theme</h1>
+    <div className='text-center'>
+    <h1 className='text-3xl font-bold bg-indigo-800 text-indigo-50 py-5'>Name : {user.name}: Age : {user.age} : Class : {user.class} </h1>
+    <button className='bg-green-600 px-3 py-2 rounded-md my-4 font-bold text-indigo-50' onClick={userInfoChange}>Change_Info_User</button>
+    <h1 className='text-3xl font-bold text-indigo-50 py-5' style={{backgroundColor : theme}} onClick={() => theme == 'red' ? setTheme('black') : setTheme('red')}>Theme</h1>
+    </div>
     </>
   )
 }
