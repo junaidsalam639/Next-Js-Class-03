9d967794e986d21ecc93888cf16ed1f1b87b8741
junaidsalam639 <junaidsalam639@gmail.com>
2023-09-28 00:21:50 +0500

    'count'

--- Next-Js-Class-03/src/app/context/ThemeContext.js
+++ Next-Js-Class-03/src/app/context/ThemeContext.js
@@ -5,9 +5,10 @@ export const Theme = createContext();
 
 export default function ThemeContext ({children}){
     const [theme , setTheme] = useState('red');
+    const [count , setCount] = useState(0);
     return(
         <>
-        <Theme.Provider value={{theme , setTheme}}>
+        <Theme.Provider value={{theme , setTheme , count , setCount}}>
             {children}
         </Theme.Provider>
         </>
--- Next-Js-Class-03/src/app/page.js
+++ Next-Js-Class-03/src/app/page.js
@@ -5,29 +5,24 @@ import { useContext } from 'react'
 export default function Home() {
   const {theme , setTheme} = useContext(Theme);
   const {user , setUser} = useContext(Auth);
-
-  const userInfoChange = () => {
-    if(user.age == 20){
-      setUser({
-        name : 'Faiz',
-        age : 25,
-        class : 'Necessary'
-      })
-    }else {
-      setUser({
-        name : 'Atta',
-        age : 20,
-        class : 'KGS-I'
-      })
-    }
+  const {count , setCount} = useContext(Theme);
+  console.log(count);
+  const Increament = () => {
+    setCount(count + 1)
   }
-  
+  const Decreament = () => {
+    setCount(count - 1)
+  }
   return (
     <>
     <div className='text-center'>
     <h1 className='text-3xl font-bold bg-indigo-800 text-indigo-50 py-5'>Name : {user.name}: Age : {user.age} : Class : {user.class} </h1>
-    <button className='bg-green-600 px-3 py-2 rounded-md my-4 font-bold text-indigo-50' onClick={userInfoChange}>Change_Info_User</button>
     <h1 className='text-3xl font-bold text-indigo-50 py-5' style={{backgroundColor : theme}} onClick={() => theme == 'red' ? setTheme('black') : setTheme('red')}>Theme</h1>
+    <h1 className='text-3xl font-bold bg-indigo-800 text-indigo-50 py-5'>
+      <h1>Count : {count}</h1>
+    <button className='bg-rose-600 px-3 py-2 my-2 rounded-md mx-2' onClick={Increament}>Count</button>
+    <button className='bg-rose-600 px-3 py-2 my-2 rounded-md mx-2' onClick={Decreament}>Count</button>
+    </h1>
     </div>
     </>
   )
